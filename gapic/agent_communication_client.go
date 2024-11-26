5d0294c07d367a10b77f5d3cbbb90abfb25c8a39
Andrew Jackura <ajackura@google.com>
2024-11-26 11:35:54 -0800

    Fix gax import rewrite
    PiperOrigin-RevId: 700409095

--- gapic/agent_communication_client.go
+++ gapic/agent_communication_client.go
@@ -21,7 +21,7 @@ import (
 	"math"
 
 	agentcommunicationpb "github.com/GoogleCloudPlatform/agentcommunication_client/gapic/agentcommunicationpb"
-	gax "google3/third_party/golang/github_com/gax/v/v2/v2/gax"
+	gax "github.com/googleapis/gax-go/v2"
 	"google.golang.org/api/option/internaloption"
 	"google.golang.org/api/option"
 	gtransport "google.golang.org/api/transport/grpc"
